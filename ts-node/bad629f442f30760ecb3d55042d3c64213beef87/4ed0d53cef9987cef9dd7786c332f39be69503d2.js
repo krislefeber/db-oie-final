7765b350b4d36778329ed14cfb62dcf57ad7c4f3
Kris Lefeber <krislefeber@gmail.com>
2017-04-28 12:45:35 -0500

    reconfigured gitignore

--- ts-node/bad629f442f30760ecb3d55042d3c64213beef87/4ed0d53cef9987cef9dd7786c332f39be69503d2.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("http");
-var app_1 = require("../app");
-var server = http.createServer(app_1.app);
-server.listen(8080);
-//# sourceMappingURL=/home/klefeber/Desktop/hessen-web/ts-node/bad629f442f30760ecb3d55042d3c64213beef87/4ed0d53cef9987cef9dd7786c332f39be69503d2.js.map
--- ts-node/caed9be88801f4a6e6796b218c793f2741a6e13e/6a27a9dffdb19d73cb728825d55b3175d7544252.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var AccountRouter = express_1.Router();
-exports.AccountRouter = AccountRouter;
-AccountRouter.get("/", function (req, res, next) {
-    res.write("getting an account?");
-});
-//# sourceMappingURL=/home/klefeber/Desktop/hessen-web/ts-node/caed9be88801f4a6e6796b218c793f2741a6e13e/6a27a9dffdb19d73cb728825d55b3175d7544252.js.map
